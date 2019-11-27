a30c3d8f713dfb3ad91a8dbb777945fdf5842847
loolzrules <loolzrules@gmail.com>
2019-11-27 23:22:08 +0600

    Added gzip compression

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+const path = require( 'path' )
 const express = require( 'express' )
 const serveStatic = require( 'serve-static' )
-const path = require( 'path' )
+const compression = require( 'compression' )
 
 const app = express()
 const defaultPort = 80
 const port = process.env.PORT || defaultPort
 const staticDir = path.join( __dirname, 'dist' )
 
+app.use( compression() )
 app.use( serveStatic( staticDir ) )
 app.get( '*', ( req, res ) => {
   res.sendFile( path.join( staticDir, 'index.html' ) )
