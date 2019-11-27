503cca9aab4d78010b305139d1fcd2ab0b1ec975
loolzrules <loolzrules@gmail.com>
2019-11-27 23:46:09 +0600

    Added button names. Edited all quotation marks, now they are consistent

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
   v-container#not-found(fill-height)
     v-layout.pb-12.text-center(justify-center, align-center, column)
-      h1 {{ $t(`views.NotFound.code`) }}
-      h2 {{ $t(`views.NotFound.main`) }}
+      h1 {{ $t( 'views.NotFound.code' ) }}
+      h2 {{ $t( 'views.NotFound.main' ) }}
 
       v-btn.info.mt-8(
-        :name='$t(`views.NotFound.return`)',
+        :name="$t( 'views.NotFound.return' )",
         x-large,
-        @click='$router.go(-1)'
-      ) {{ $t(`views.NotFound.return`) }}
+        @click="$router.go(-1)"
+      ) {{ $t( 'views.NotFound.return' ) }}
 
 </template>
 
