74a4aae29149a51930fd5dbc693b1bc54bdf69b6
loolzrules <loolzrules@gmail.com>
2019-11-27 23:04:59 +0600

    Added support for history mode on the server side. Changed mode to production

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const path = require( 'path' )
 const app = express()
 const defaultPort = 80
 const port = process.env.PORT || defaultPort
+const staticDir = path.join( __dirname, 'dist' )
 
-app.use( serveStatic( path.join( __dirname, 'dist' ) ) )
+app.use( serveStatic( staticDir ) )
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.join( staticDir, 'index.html' ) )
+} )
 app.listen( port )
